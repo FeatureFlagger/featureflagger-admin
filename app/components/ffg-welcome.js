962716d43462dc46601713b70da77170d54bebb1
Aurelio Saraiva <aureliosaraiva@gmail.com>
2017-04-01 18:12:03 -0300

    remove code not used

--- app/components/ffg-welcome.js
+++ app/components/ffg-welcome.js
@@ -1,9 +1,3 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
-
-  errorClass() {
-    const error = true;
-    return error ? 'success': 'error';
-  }
-});
+export default Ember.Component.extend({});
