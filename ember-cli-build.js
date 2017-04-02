ccff393b8929ef610322d65917753ca30e37f988
Aurelio Saraiva <aureliosaraiva@gmail.com>
2017-04-02 14:20:07 -0300

    structure default for css

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,23 +1,45 @@
 /* eslint-env node */
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const environment = EmberApp.env();
+const isProduction = environment === 'production';
+
+function postcssPlugins() {
+    var plugins = [
+      {
+        module: require('postcss-easy-import')
+      },
+      {
+        module: require('postcss-custom-properties')
+      },
+      {
+        module: require('postcss-color-function')
+      },
+      {
+        module: require('autoprefixer'),
+        options: {
+            browsers: ['last 2 versions']
+        }
+      }
+  ];
+
+    if (isProduction) {
+        plugins.push({
+            module: require('cssnano')
+        });
+    }
+
+    return plugins;
+}
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    postcssOptions: {
+        compile: {
+            enabled: true,
+            plugins: postcssPlugins()
+        }
+    },
   });
-
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
 
   return app.toTree();
 };
