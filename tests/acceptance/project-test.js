e987a19227121448c84ea4d4ddb8c4e627c36d95
Aurelio Saraiva <aureliosaraiva@gmail.com>
2017-04-02 16:49:28 -0300

    create test of acceptance

---
+++ tests/acceptance/project-test.js
@@ -0,0 +1,45 @@
+/* jshint expr:true */
+import {
+    describe,
+    it,
+    beforeEach,
+    afterEach
+} from 'mocha';
+import {expect} from 'chai';
+import startApp from '../helpers/start-app';
+import destroyApp from '../helpers/destroy-app';
+import testSelector from 'ember-test-selectors';
+
+
+describe('Acceptance: Project', function () {
+    let application;
+
+    beforeEach(function () {
+        application = startApp();
+    });
+
+    afterEach(function () {
+        destroyApp(application);
+    });
+
+    it('working', function () {
+        visit('/project');
+
+        andThen(function () {
+            expect(currentURL(), 'currentURL').to.equal('/project');
+        });
+    });
+
+    it('list project', function () {
+        visit('/project');
+
+        andThen(function () {
+            expect(currentURL(), 'currentURL').to.equal('/project');
+
+            expect(
+                find(`${testSelector('project-actives')} ${testSelector('project-item')}`).length,
+                'number of active projects'
+            ).to.equal(3);
+        });
+    });
+});
