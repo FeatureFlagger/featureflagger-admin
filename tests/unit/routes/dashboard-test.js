befdb69f7c8e43db52c46da8315b4908a5be9370
Aurelio Saraiva <aureliosaraiva@gmail.com>
2017-04-04 19:36:33 -0300

    fix test

--- tests/unit/routes/dashboard-test.js
+++ tests/unit/routes/dashboard-test.js
@@ -5,7 +5,9 @@ import { setupTest } from 'ember-mocha';
 describe('Unit | Route | dashboard', function() {
   setupTest('route:dashboard', {
     // Specify the other units that are required for this test.
-    // needs: ['controller:foo']
+    needs: [
+      'service:session'
+    ]
   });
 
   it('exists', function() {
--- tests/unit/routes/signout-test.js
+++ tests/unit/routes/signout-test.js
@@ -5,7 +5,9 @@ import { setupTest } from 'ember-mocha';
 describe('Unit | Route | signout', function() {
   setupTest('route:signout', {
     // Specify the other units that are required for this test.
-    // needs: ['controller:foo']
+    needs: [
+      'service:session'
+    ]
   });
 
   it('exists', function() {
